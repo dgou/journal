61d93c0f67d5e2d88b89dca6b63b43ceb9a35e48
Doug Philips <douglas.philips@gmail.com>
2011-08-16 18:23:31 -0400

    -e for editing the journal. If an entry is given (-l or cmd line) add it first.

--- journal/main.py
+++ journal/main.py
@@ -3,11 +3,13 @@
 
 import sys
 import argparse
-from os import path, makedirs
+import subprocess
+from os import path, makedirs, environ
 from datetime import datetime
 
 from journal import __version__
 
+DEFAULT_EDITOR = "vim"
 JOURNAL_ROOT = path.expanduser("~")
 JOURNAL_DEST = path.join(JOURNAL_ROOT, ".journal")
 JOURNAL_ENTRY_FORMAT = "%Y.%m.%d %I:%M:%S %a"
@@ -34,6 +36,9 @@ def parse_args():
     parser.add_argument('-l', '--long',
                         action="store_true",
                         help='long entry. Read entry from stdin instead of command line.')
+    parser.add_argument("-e", "--edit",
+                        action="store_true",
+                        help="edit the journal log file after appending new entry.")
     parser.add_argument('entry',
             nargs='*',
             help="Text to make an entry in your journal")
@@ -71,8 +76,13 @@ def show_entry(date):
 def show_today():
     return show_entry(datetime.today())
 
+def do_edit(file_name):
+    editor = environ.get("VISUAL") or environ.get("EDITOR") or DEFAULT_EDITOR
+    subprocess.call([editor, file_name])
+   
 def main():
     args, help_string = parse_args()
+    journal_file_name = build_journal_path(datetime.today())
 
     if args.today:
         entry = show_today()
@@ -82,13 +92,20 @@ def main():
             print "journal: error: entry not found for today"
             sys.exit()
     elif args.file:
-        print build_journal_path(datetime.today())
+        print journal_file_name
         sys.exit()
     elif args.long:
         record_entry(get_stdin_entry())
-    elif args.entry:
+        if args.edit:
+            do_edit(journal_file_name)
+        sys.exit()
+
+    if args.entry:
         record_entry(args.entry)
-    else:
+
+    if args.edit:
+        do_edit(journal_file_name)
+    elif not args.entry:
         print help_string
         sys.exit()
 
